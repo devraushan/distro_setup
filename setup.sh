6d4554f3a0d5a4043a7fe80c79dd618f0da61a56
DevRaushan <devraushansingh@gmail.com>
2023-09-18 23:15:24 -0400

    first commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+git config --global user.name "DevRaushan"
+git config --global user.email "devraushansingh@gmail.com"
+git config --global init.defaultBranch main
+git config --global color.ui auto
+git config --global pull.rebase false
+ssh-keygen -t ed25519 -C devraushansingh@gmail.com
+cat ~/.ssh/id_ed25519.pub
